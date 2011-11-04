bb2a2dd3ac7d520436335b53d4322c9461cb8991
Beth <eam74@case.edu>
2011-11-03 20:27:02 -0400

    added template files

---
+++ footer.php
@@ -0,0 +1,7 @@
+<!-- Begin footer -->
+</div>
+<div id="footer">
+	&copy; 2011 and stuff like that
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include("header.php"); ?>
+
+<p>This is an example page, this is the main text WOO</p>
+
+<?php include("footer.php"); ?>
