28ee81cb8b2091ef198f282368a9159241d2d70e
Beth <eam74@case.edu>
2011-11-10 14:14:11 -0500

    finishing up some db api stuff

--- phpincludes/db_equipment.php
+++ phpincludes/db_equipment.php
@@ -4,7 +4,7 @@
 
 function addEquipment($type, $serialnum, $brand, $owner)
 {
-	$querystr = "INSERT INTO equipment (type, serialnum, brand, owner) VALUES (" +
+	$querystr = "INSERT INTO inventory (type, serialnum, brand, owner) VALUES (" +
 		$type + "," +
 		$serialnum + "," +
 		$brand + "," +
@@ -12,7 +12,7 @@ function addEquipment($type, $serialnum, $brand, $owner)
 	run_query($querystr);
 	
 	// return eid
-	$querystr = "SELECT eid FROM equipment WHERE" +
+	$querystr = "SELECT eid FROM inventory WHERE" +
 		"type=" + $type +
 		" AND serialnum=" + $serialnum +
 		" AND brand=" + $brand +
@@ -72,8 +72,65 @@ function addRiser($type, $serialnum, $brand, $owner, $interface, $height, $disti
 	run_query($querystr);
 }
 
-//TODO: editEquipment, editStabilizer, editLimb, editArrow, editRiser
+// Edit Functions
+function editEquipment($eid, $type, $serialnum, $brand, $owner)
+{
+	$querystr = "UPDATE inventory SET " +
+		" type=" + $type +
+		" serialnum=" + $serialnum +
+		" brand=" + $brand +
+		" owner=" + $owner +
+		" WHERE eid=" + $eid;
+	run_query($querystr);
+}
 
+function editStabilizer($eid, $type, $serialnum, $brand, $owner, $length)
+{
+	$eid = editEquipment($eid, $type, $serialnum, $brand, $owner);
+	$querystr = "UPDATE stabilizer SET length=" + $length + " WHERE eid=" + $eid;
+	run_query($querystr);
+}
+
+function editLimb($eid, $type, $serialnum, $brand, $owner, $interface, $distinguishing_marks, $draw_strength)
+{
+	$eid = editEquipment($eid, $type, $serialnum, $brand, $owner);
+	$querystr = "UPDATE limb SET " +
+		" interface=" + $interface + 
+		" distinguishing_marks=" +$distinguishing_marks + 
+		" draw_strength=" +$draw_strength +
+		" WHERE eid=" + $eid;
+	run_query($querystr);
+}
+
+function editArrow($eid, $type, $serialnum, $brand, $owner, $model, $complete_arrow, $bare_shaft, $fixable, $notes)
+{
+	$eid = editEquipment($eid, $type, $serialnum, $brand, $owner);
+	$querystr = "UPDATE arrow SET " +
+		" model=" + $model + 
+		" complete_arrow=" +$complete_arrow + 
+		" bare_shaft=" +$bare_shaft + 
+		" fixable=" +$fixable + 
+		" notes=" +$notes +
+		" WHERE eid=" + $eid;
+	run_query($querystr);
+}
+
+function editRiser($eid, $type, $serialnum, $brand, $owner, $interface, $height, $distinguishing_marks, $handedness, $button_format, $button, $arrow_rest)
+{
+	$eid = editEquipment($eid, $type, $serialnum, $brand, $owner);
+	$querystr = "UPDATE riser SET " +
+		" interface=" + $interface + 
+		" height=" + $height + 
+		" distinguishing_marks=" + $distinguishing_marks + 
+		" handedness=" + $handedness + 
+		" button_format=" + $button_format + 
+		" button=" + $button + 
+		" arrow_rest=" + $arrow_rest +
+		" WHERE eid=" + $eid;
+	run_query($querystr);
+}
+
+// Other functions
 function deleteEquipment($eid)
 {
 	$querystr = "DELETE FROM equipment WHERE eid=" + $eid;
---
+++ phpincludes/db_members.php
@@ -0,0 +1,148 @@
+<?php
+// Functions regarding members
+// run_query from include("db_connect.php");
+
+// Contacts
+function addContact($first_name, $last_name, $address, $city, $state, $zip, $email, $phone)
+{
+	$querystr = "INSERT INTO contact (first_name, last_name, address, city, state, zip, email, phone) VALUES (" +TT
+		$first_name + ", " +
+		$last_name + ", " +
+		$address + ", " +
+		$city + ", " +
+		$state + ", " +
+		$zip + ", " +
+		$email + ", " +
+		$phone + ")";
+	run_query($querystr);
+}
+
+function editContact($pid, $first_name, $last_name, $address, $city, $state, $zip, $email, $phone)
+{
+	$querystr = "UPDATE contact SET " +
+		"first_name=" + $first_name +
+		", last_name=" + $last_name +
+		", address=" + $address +
+		", city=" + $city +
+		", state=" + $state +
+		", zip=" + $zip+
+		", email=" + $email +
+		", phone=" + $phone +
+		" WHERE pid=" + $pid;
+	run_query($querystr);
+}
+
+function deleteContact($pid)
+{
+	$querystr = "DELETE FROM contact WHERE pid=" + $pid;
+	run_query($querystr);
+}
+
+// Members
+function addMember($pid, $netid, $studentid, $bow_preference, $handedness, $membership_expiration, $usca_id, $usca_expiration, $emergency_name, $emergency_phone, $insurance_company, $policy_number)
+{
+	$querystr = "INSERT INTO member (pid, netid, studentid, bow_preference, handedness, membership_expiration, usca_id, usca_expiration, emergency_name, emergency_phone, insurance_company, policy_number) VALUES ( " +
+		$pid + ", " +
+		$netid + ", " +
+		$studentid+ ", " +
+		$bow_preference + ", " +
+		$handedness + ", " +
+		$membership_expiration + ", " +
+		$usca_id + ", " +
+		$usca_expiration + ", " +
+		$emergency_name + ", " +
+		$emergency_phone + ", " +
+		$insurance_company + ", " +
+		$policy_number + ")";
+	run_query($querystr);
+}
+
+function editMember($mid, $netid, $studentid, $bow_preference, $handedness, $membership_expiration, $usca_id, $usca_expiration, $emergency_name, $emergency_phone, $insurance_company, $policy_number)
+{
+	$querystr = "UPDATE member SET "
+		" netid=" + $netid + 
+		" studentid=" + $studentid + 
+		" bow_preference=" + $bow_preference + 
+		" handedness=" + $handedness + 
+		" membership_expiration=" + $membership_expiration + 
+		" usca_id=" + $usca_id + 
+		" usca_expiration=" + $usca_expiration + 
+		" emergency_name=" + $emergency_name + 
+		" emergency_phone=" + $emergency_phone + 
+		" insurance_company=" + $insurance_company + 
+		" policy_number=" + $policy_number +
+		" WHERE mid=" + $mid;
+	run_query($querystr);
+}
+
+function deleteMember($mid)
+{
+	$querystr = "DELETE FROM member WHERE mid=" + $mid;
+	run_query($querystr);
+}
+
+// Certifications
+function addCertification($pid, $type, $expiration, $notes)
+{
+	$querystr = "INSERT INTO certification (pid, type, expiration, notes) VALUES (" +
+		$pid + ", " +
+		$type + ", " +
+		$expiration + ", " +
+		$notes + ")";
+	run_query($querystr);
+}
+
+function editCertification($cid, $pid, $type, $expiration, $notes)
+{
+	$querystr = "UPDATE certification SET " +
+		" pid=" + $pid +
+		" type=" + $type +
+		" expiration=" + expiration +
+		" notes=" + notes +
+		" WHERE cid=" + $cid;
+	run_query($querystr);
+}
+
+function deleteCertification($cid)
+{
+	$querystr = "DELETE FROM certification WHERE cid=" + $cid;
+	run_query($querystr);
+}
+
+// Scores
+
+function addScore($pid, $total, $location, $score_date, $division, $scorecard, $tens, $nines)
+{
+	$querystr = "INSERT INTO score (pid, total, location, score_date, division, scorecard, tens, nines) VALUES (" +
+		$pid + ", " +
+		$total + ", " +
+		$location + ", " +
+		$score_date + ", " +
+		$division + ", " +
+		$scorecard + ", " +
+		$tens + ", " +
+		$nines + ")";
+	run_query($querystr);
+}
+
+function editScore($sid, $pid, $total, $location, $score_date, $division, $scorecard, $tens, $nines)
+{
+	$querystr = "UPDATE score SET " +
+		" pid=" + $pid +
+		" total=" + $total +
+		" location=" + $location +
+		" score_date=" + $score_date +
+		" division=" + $division +
+		" scorecard=" + $scorecard +
+		" tens=" + $tens +
+		" nines=" + $nines +
+		" WHERE sid=" + $sid;
+	run_query($querystr);
+}
+
+function deleteScore($sid)
+{
+	$querystr = "DELETE FROM score WHERE sid=" + $sid;
+	run_query($querystr);
+}
+?>
