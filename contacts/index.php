3809f94f49dc08b792291bade258e9e5d9e43007
wes <wes@1337-m4ch1n3.(none)>
2011-12-13 01:52:50 -0500

    working on members and contacts

--- contacts/index.php
+++ contacts/index.php
@@ -18,29 +18,17 @@ HERE;
        
         //show contacts
         $members = db_access::getContact(NULL);
-        $row = mysqli_fetch_row($equiplist);
+        
 
         $admin = db_access::isAdmin($pid);
 
-        echo "<table><tr><th> </th><th>Name</th><th>Address</th><th>e-mail address</th><th>Phone</th><th> </th></tr>";
-        while($row)
+        echo "<table><tr><th>Name</th><th>Address</th><th>e-mail address</th><th>Phone</th><th> </th></tr>";
+        while($row=mysqli_fetch_row($members))
         {
-            echo "<td> ";
-            if($admin)
-            {
-                 echo "<form action='editcontact.php' method='POST'>";
-                 echo "<input type='hidden' name='pid' value='".$row[0]."' />";
-                 echo "<input type='submit' value='Edit' name='editcon'></form>";
-
-            }
-            echo "</td>";
             echo "<td>".$row[1]." ".$row[2]."</td><td>".$row[3]." ".$row[4].", ".$row[5]." ".$row[6]."</td><td>".$row[7]."</td><td>".$row[8]."</td><td>";
-            if($admin&&$pid!=$row[0])
-            {
-                 echo "<form action='delcontact.php' method='POST'>";
+                 echo "<form action='contactdetails.php' method='POST'>";
                  echo "<input type='hidden' name='pid' value='".$row[0]."' />";
-                 echo "<input type='submit' value='Delete' name='delmem'></form>";
-            }
+                 echo "<input type='submit' value='Details' name='details'></form>";
             echo "</td>";
         }
         echo "</table>";
--- membership/index.php
+++ membership/index.php
@@ -8,7 +8,7 @@
        In order for the club to keep track of contacts, there will be a contacts table of all of the people that are in the club records including the person’s name, a unique PID (Person IDentifier), and some basic contact information (address, e-mail, phone).  This portion of the database may contain both members and non-members. It is simply a way of associating other records with a person, and also a way for club members to look up contact info. This PID field is a primary key that points back to a particular person and will often be used as a foreign key elsewhere in the database.
         */
 ?>
-<h2>Contact Info</h2>
+<h2>Members</h2>
 <?php if (!db_access::isMember(db_access::getPidFromCaseId(phpCAS::getUser()))) {
 	echo <<<HERE
 	<div class='error'>Sorry, you are not authorized to view this page.</div>
@@ -16,31 +16,21 @@ HERE;
 } else { 
         $pid = db_access::getPidFromCaseId(phpCas::getUser());
        
-        //show contacts
-        $members = db_access::getContact(NULL);
-        $row = mysqli_fetch_row($equiplist);
+        //show members
+        
+ 
+        $members = db_access::getMember(NULL);
+        $row = mysqli_fetch_row($members);
 
         $admin = db_access::isAdmin($pid);
 
-        echo "<table><tr><th> </th><th>Name</th><th>Address</th><th>e-mail address</th><th>Phone</th><th> </th></tr>";
+        echo "<table><tr><th>Case ID</th><th>Student ID</th><th> </th>";
         while($row)
         {
-            echo "<td> ";
-            if($admin)
-            {
-                 echo "<form action='editcontact.php' method='POST'>";
+            echo "<td>".$row[2]"</td><td>"$row[3]"</td><td>";
+                 echo "<form action='memberdetails.php' method='POST'>";
                  echo "<input type='hidden' name='pid' value='".$row[0]."' />";
-                 echo "<input type='submit' value='Edit' name='editcon'></form>";
-
-            }
-            echo "</td>";
-            echo "<td>".$row[1]." ".$row[2]."</td><td>".$row[3]." ".$row[4].", ".$row[5]." ".$row[6]."</td><td>".$row[7]."</td><td>".$row[8]."</td><td>";
-            if($admin&&$pid!=$row[0])
-            {
-                 echo "<form action='delcontact.php' method='POST'>";
-                 echo "<input type='hidden' name='pid' value='".$row[0]."' />";
-                 echo "<input type='submit' value='Delete' name='delmem'></form>";
-            }
+                 echo "<input type='submit' value='Details' name='details'></form>";
             echo "</td>";
         }
         echo "</table>";
